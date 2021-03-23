355ddab236f228f53b1a019447d4c135d7102159
Amanda <amandaben21@aol.com>
2021-03-23 18:51:42 -0400

    Done

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+    has_many :recipe_ingredients
+    has_many :ingredients, through: :recipe_ingredients
+    
+    validates :name, presence: true
+    
+    accepts_nested_attributes_for :ingredients
+    
+    def ingredient_attributes(ingredient_ids)
+        ingredient_ids.values.each do |ii|
+            ingredient = Ingredient.find_or_create_by(ii)
+            self.ingredients << ingredient
+        end
+    end
 end
